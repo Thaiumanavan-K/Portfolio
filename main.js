afb55dfa92bb58e7ac588f535a28448b13a2fbc0
Thaiumanavan-K <thaiumanavank@gmail.com>
2026-04-02 17:09:44 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,56 @@
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
+
+window.addEventListener('load', function() {
+    window.scrollTo(0, 0);
+    if (window.location.hash) {
+        window.history.replaceState(null, null, window.location.pathname + window.location.search);
+    }
+});
+
+var textlinks = document.getElementsByClassName("text-links");
+var aboutcontents = document.getElementsByClassName("about-content");
+
+function opentab(tabname){
+    for(let textlink of textlinks){
+        textlink.classList.remove("active-link");
+    }
+    for(let aboutcontent of aboutcontents){
+        aboutcontent.classList.remove("active-content");
+    }
+
+    if (window.event && window.event.currentTarget) {
+        window.event.currentTarget.classList.add("active-link");
+    }
+    document.getElementById(tabname).classList.add("active-content");
+}
+
+var currentPages = document.getElementsByClassName("current-page");
+
+function openSection(){
+    for(let currentPage of currentPages){
+        currentPage.classList.remove("active-content");
+    }
+    if (window.event && window.event.currentTarget) {
+        window.event.currentTarget.classList.add("active-content");
+    }
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyUL36BU9D8O2WU85-vxiTzYB9s49MKi1qaNRFg5ocQkl0cIKehrrgcbtySems-3Y40/exec'
+const form = document.forms['Google-Form']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "<p><i class='bx bxs-message-square-check'></i> Message Sent Successfully</p>";
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },2000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
